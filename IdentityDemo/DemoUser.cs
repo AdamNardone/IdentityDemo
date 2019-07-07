a8d5b07b67d8997975e87b4d7eaca3b28f0529df
nardoneadam <nardoneadam@gmail.com>
2019-07-07 13:23:30 -0400

    Created user entity, userstore and database.

---
+++ IdentityDemo/DemoUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityDemo
+{
+    public class DemoUser
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string NormalizedUsername { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
--- IdentityDemo/Startup.cs
+++ IdentityDemo/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -33,6 +34,8 @@ namespace IdentityDemo
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddIdentityCore<string>(options => { });
+            services.AddScoped<IUserStore<DemoUser>, DemoUserStore>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
